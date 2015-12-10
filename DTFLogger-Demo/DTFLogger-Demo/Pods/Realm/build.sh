c4abf56f664d0b22ae26a3c4f4391003fdec4a28
Darren Ferguson <darren102@gmail.com>
2015-12-10 10:58:37 -0500

    Upgrading realm to latest version

--- DTFLogger-Demo/DTFLogger-Demo/Pods/Realm/build.sh
+++ DTFLogger-Demo/DTFLogger-Demo/Pods/Realm/build.sh
@@ -14,12 +14,12 @@ set -o pipefail
 set -e
 
 # You can override the version of the core library
-: ${REALM_CORE_VERSION:=0.92.4} # set to "current" to always use the current build
+: ${REALM_CORE_VERSION:=0.95.4} # set to "current" to always use the current build
 
 # You can override the xcmode used
 : ${XCMODE:=xcodebuild} # must be one of: xcodebuild (default), xcpretty, xctool
 
-PATH=/usr/local/bin:/usr/bin:/bin:/usr/libexec:$PATH
+PATH=/usr/libexec:$PATH
 
 if ! [ -z "${JENKINS_HOME}" ]; then
     XCPRETTY_PARAMS="--no-utf --report junit --output build/reports/junit.xml"
@@ -49,7 +49,9 @@ command:
   test-ios-static:      tests static iOS framework on 32-bit and 64-bit simulators
   test-ios-dynamic:     tests dynamic iOS framework on 32-bit and 64-bit simulators
   test-ios-swift:       tests RealmSwift iOS framework on 32-bit and 64-bit simulators
-  test-ios-devices:     tests dynamic and Swift iOS frameworks on all attached iOS devices
+  test-ios-devices:     tests ObjC & Swift iOS frameworks on all attached iOS devices
+  test-ios-devices-objc:  tests ObjC iOS framework on all attached iOS devices
+  test-ios-devices-swift: tests Swift iOS framework on all attached iOS devices
   test-osx:             tests OS X framework
   test-osx-swift:       tests RealmSwift OS X framework
   verify:               verifies docs, osx, osx-swift, ios-static, ios-dynamic, ios-swift, ios-device in both Debug and Release configurations
@@ -86,6 +88,8 @@ xcode() {
 }
 
 xc() {
+    # Logs xcodebuild output in realtime
+    : ${NSUnbufferedIO:=YES}
     if [[ "$XCMODE" == "xcodebuild" ]]; then
         xcode "$@"
     elif [[ "$XCMODE" == "xcpretty" ]]; then
@@ -97,6 +101,10 @@ xc() {
     elif [[ "$XCMODE" == "xctool" ]]; then
         xctool "$@"
     fi
+}
+
+copy_bcsymbolmap() {
+    find "$1" -name '*.bcsymbolmap' -type f -exec cp {} "$2" \;
 }
 
 xcrealm() {
@@ -133,6 +141,9 @@ build_ios_combined() {
     if [ -d $iphonesimulator_path/Modules/$module_name.swiftmodule ]; then
       cp $iphonesimulator_path/Modules/$module_name.swiftmodule/* $iphoneos_path/Modules/$module_name.swiftmodule/
     fi
+    
+    # Copy *.bcsymbolmap to .framework for submitting app with bitcode
+    copy_bcsymbolmap "$build_products_path/$config-iphoneos$scope_suffix" "$iphoneos_path"
 
     # Retrieve build products
     clean_retrieve $iphoneos_path $out_path $product_name
@@ -164,6 +175,9 @@ build_watchos_combined() {
     if [ -d $watchsimulator_path/Modules/$module_name.swiftmodule ]; then
       cp $watchsimulator_path/Modules/$module_name.swiftmodule/* $watchos_path/Modules/$module_name.swiftmodule/
     fi
+    
+    # Copy *.bcsymbolmap
+    copy_bcsymbolmap "$build_products_path/$config-watchos$scope_suffix" "$watchos_path"
 
     # Retrieve build products
     clean_retrieve $watchos_path $out_path $product_name
@@ -221,6 +235,44 @@ test_ios_devices() {
         $cmd "-scheme '$2' -configuration $configuration -destination 'id=$device' test" || failed=1
     done
     return $failed
+}
+
+######################################
+# Docs
+######################################
+
+build_docs() {
+    local language="$1"
+    local version=$(sh build.sh get-version)
+
+    local xcodebuild_arguments="--objc,Realm/Realm.h,-x,objective-c,-isysroot,$(xcrun --show-sdk-path),-I,$(pwd)"
+    local module="Realm"
+    local objc="--objc"
+
+    if [[ "$language" == "swift" ]]; then
+        : ${REALM_SWIFT_VERSION:=2.1}
+        ./build.sh set-swift-version
+        xcodebuild_arguments="-project,RealmSwift.xcodeproj,-scheme,RealmSwift"
+        module="RealmSwift"
+        objc=""
+    fi
+
+    touch Realm/RLMPlatform.h # jazzy will fail if it can't find all public header files
+    jazzy \
+      ${objc} \
+      --clean \
+      --author Realm \
+      --author_url https://realm.io \
+      --github_url https://github.com/realm/realm-cocoa \
+      --github-file-prefix https://github.com/realm/realm-cocoa/tree/v${version} \
+      --module-version ${version} \
+      --xcodebuild-arguments ${xcodebuild_arguments} \
+      --module ${module} \
+      --root-url https://realm.io/docs/${language}/${version}/api/ \
+      --output $(pwd)/docs/${language}_output \
+      --template-directory $(pwd)/docs/templates
+
+    rm Realm/RLMPlatform.h
 }
 
 ######################################
@@ -437,38 +489,49 @@ case "$COMMAND" in
         ;;
 
     "test-ios-static")
-        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 6' test"
+        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 6' build test"
         shutdown_simulators
-        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s' test"
+        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s' build test"
         exit 0
         ;;
 
     "test-ios7-static")
-        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 5S,OS=7.1' test"
+        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 5S,OS=7.1' build test"
         shutdown_simulators
-        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s,OS=7.1' test"
+        xcrealm "-scheme iOS -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s,OS=7.1' build test"
         exit 0
         ;;
 
     "test-ios-dynamic")
-        xcrealm "-scheme 'iOS Dynamic' -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 6' test"
+        xcrealm "-scheme 'iOS Dynamic' -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 6' build test"
         shutdown_simulators
-        xcrealm "-scheme 'iOS Dynamic' -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s' test"
+        xcrealm "-scheme 'iOS Dynamic' -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s' build test"
         exit 0
         ;;
 
     "test-ios-swift")
-        xcrealmswift "-scheme RealmSwift -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 6' test"
+        xcrealmswift "-scheme RealmSwift -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 6' build test"
         shutdown_simulators
-        xcrealmswift "-scheme RealmSwift -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s' test"
+        xcrealmswift "-scheme RealmSwift -configuration $CONFIGURATION -sdk iphonesimulator -destination 'name=iPhone 4s' build test"
         exit 0
         ;;
 
     "test-ios-devices")
         failed=0
-        test_ios_devices xcrealm "iOS Device Tests" "$CONFIGURATION" || failed=1
-        # test_ios_devices xcrealmswift "RealmSwift" "$CONFIGURATION" || failed=1 # FIXME: Re-enable once fixed
+        trap "failed=1" ERR
+        sh build.sh test-ios-devices-objc
+        sh build.sh test-ios-devices-swift
         exit $failed
+        ;;
+
+    "test-ios-devices-objc")
+        test_ios_devices xcrealm "iOS Device Tests" "$CONFIGURATION"
+        exit $?
+        ;;
+
+    "test-ios-devices-swift")
+        test_ios_devices xcrealmswift "RealmSwift" "$CONFIGURATION"
+        exit $?
         ;;
 
     "test-osx")
@@ -489,6 +552,7 @@ case "$COMMAND" in
     # Full verification
     ######################################
     "verify")
+        sh build.sh verify-cocoapods
         sh build.sh verify-docs
         sh build.sh verify-osx
         sh build.sh verify-osx-debug
@@ -502,8 +566,16 @@ case "$COMMAND" in
         sh build.sh verify-ios-dynamic-debug
         sh build.sh verify-ios-swift
         sh build.sh verify-ios-swift-debug
-        sh build.sh verify-ios-device
+        sh build.sh verify-ios-device-objc
+        sh build.sh verify-ios-device-swift
         sh build.sh verify-watchos
+        ;;
+
+    "verify-cocoapods")
+        cd examples/installation
+        # FIXME: tests are duplicated to work around https://github.com/realm/realm-cocoa/issues/2701
+        sh build.sh test-ios-objc-cocoapods || sh build.sh test-ios-objc-cocoapods || exit 1
+        sh build.sh test-ios-swift-cocoapods || sh build.sh test-ios-swift-cocoapods || exit 1
         ;;
 
     "verify-osx")
@@ -540,18 +612,26 @@ case "$COMMAND" in
         sh build.sh examples-ios-swift
         ;;
 
-    "verify-ios-device")
-        sh build.sh test-ios-devices
+    "verify-ios-device-objc")
+        sh build.sh test-ios-devices-objc
+        exit 0
+        ;;
+
+    "verify-ios-device-swift")
+        sh build.sh test-ios-devices-swift
         exit 0
         ;;
 
     "verify-docs")
-        sh scripts/build-docs.sh
-        if [ -s docs/swift_output/undocumented.txt ]; then
-          echo "Undocumented RealmSwift declarations:"
-          echo "$(cat docs/swift_output/undocumented.txt)"
-          exit 1
-        fi
+        sh build.sh docs
+        for lang in swift objc; do
+            undocumented="docs/${lang}_output/undocumented.txt"
+            if [ -s "$undocumented" ]; then
+              echo "Undocumented Realm $lang declarations:"
+              cat "$undocumented"
+              exit 1
+            fi
+        done
         exit 0
         ;;
 
@@ -562,11 +642,16 @@ case "$COMMAND" in
         exit 0
         ;;
 
+    "verify-swiftlint")
+        exit 0
+        ;;
+
     ######################################
     # Docs
     ######################################
     "docs")
-        sh scripts/build-docs.sh
+        build_docs objc
+        build_docs swift
         exit 0
         ;;
 
@@ -664,7 +749,7 @@ case "$COMMAND" in
           if [[ -L "$symlink" ]]; then
             link="$(dirname "$symlink")/$(readlink "$symlink")"
             rm "$symlink"
-            cp -R "$link" "$symlink"
+            cp -RH "$link" "$symlink"
           fi
         done
 
@@ -672,11 +757,48 @@ case "$COMMAND" in
           # CocoaPods doesn't support multiple header_mappings_dir, so combine
           # both sets of headers into a single directory
           rm -rf include
+          # Create uppercase `Realm` header directory for a case-sensitive filesystem.
+          # Both `Realm` and `realm` directories are required.
+          if [ ! -e core/include/Realm ]; then
+            cp -R core/include/realm core/include/Realm
+          fi
           cp -R core/include include
           mkdir -p include/Realm
           cp Realm/*.{h,hpp} include/Realm
           cp Realm/ObjectStore/*.hpp include/Realm
           touch include/Realm/RLMPlatform.h
+        fi
+        ;;
+
+    ######################################
+    # Continuous Integration
+    ######################################
+
+    "ci-pr")
+        mkdir -p build/reports
+
+        if [ "$target" = "docs" ]; then
+            sh build.sh set-swift-version
+            sh build.sh verify-docs
+        elif [ "$target" = "swiftlint" ]; then
+            sh build.sh verify-swiftlint
+        else
+            export sha=$ghprbSourceBranch
+            export REALM_SWIFT_VERSION=$swift_version
+            export CONFIGURATION=$configuration
+            export REALM_EXTRA_BUILD_ARGUMENTS='GCC_GENERATE_DEBUGGING_SYMBOLS=NO REALM_PREFIX_HEADER=Realm/RLMPrefix.h'
+            sh build.sh prelaunch-simulator
+            # Verify that no Realm files still exist
+            ! find ~/Library/Developer/CoreSimulator/Devices/ -name '*.realm' | grep -q .
+
+            sh build.sh verify-$target | tee build/build.log | xcpretty -r junit -o build/reports/junit.xml || \
+                (echo "\n\n***\nbuild/build.log\n***\n\n" && cat build/build.log && exit 1)
+        fi
+
+        if [ "$target" = "osx" ] && [ "$configuration" = "Debug" ]; then
+          gcovr -r . -f ".*Realm.*" -e ".*Tests.*" -e ".*core.*" --xml > build/reports/coverage-report.xml
+          WS=$(pwd | sed "s/\//\\\\\//g")
+          sed -i ".bak" "s/<source>\./<source>${WS}/" build/reports/coverage-report.xml
         fi
         ;;
 
@@ -691,27 +813,31 @@ case "$COMMAND" in
         ;;
 
     "package-test-examples")
-        VERSION=$(file realm-objc-*.zip | grep -o '\d*\.\d*\.\d*')
-        unzip realm-objc-${VERSION}.zip
+        if ! VERSION=$(echo realm-objc-*.zip | grep -o '\d*\.\d*\.\d*-[a-z]*'); then
+            VERSION=$(echo realm-objc-*.zip | grep -o '\d*\.\d*\.\d*')
+        fi
+        OBJC="realm-objc-${VERSION}"
+        SWIFT="realm-swift-${VERSION}"
+        unzip ${OBJC}.zip
 
-        cp $0 realm-objc-${VERSION}
-        cp -r $(dirname $0)/scripts realm-objc-${VERSION}
-        cd realm-objc-${VERSION}
+        cp $0 ${OBJC}
+        cp -r $(dirname $0)/scripts ${OBJC}
+        cd ${OBJC}
         REALM_SWIFT_VERSION=1.2 sh build.sh examples-ios
-        REALM_SWIFT_VERSION=2.0 sh build.sh examples-ios
+        REALM_SWIFT_VERSION=2.1 sh build.sh examples-ios
         sh build.sh examples-osx
         cd ..
-        rm -rf realm-objc-${VERSION}
+        rm -rf ${OBJC}
 
-        unzip realm-swift-${VERSION}.zip
+        unzip ${SWIFT}.zip
 
-        cp $0 realm-swift-${VERSION}
-        cp -r $(dirname $0)/scripts realm-swift-${VERSION}
-        cd realm-swift-${VERSION}
+        cp $0 ${SWIFT}
+        cp -r $(dirname $0)/scripts ${SWIFT}
+        cd ${SWIFT}
         REALM_SWIFT_VERSION=1.2 sh build.sh examples-ios-swift
-        REALM_SWIFT_VERSION=2.0 sh build.sh examples-ios-swift
+        REALM_SWIFT_VERSION=2.1 sh build.sh examples-ios-swift
         cd ..
-        rm -rf realm-swift-${VERSION}
+        rm -rf ${SWIFT}
         ;;
 
     "package-ios-static")
@@ -723,9 +849,9 @@ case "$COMMAND" in
         move_to_clean_dir build/ios/Realm.framework xcode-6
         rm -rf build
 
-        REALM_SWIFT_VERSION=2.0 sh build.sh prelaunch-simulator
-        REALM_SWIFT_VERSION=2.0 sh build.sh test-ios-static
-        REALM_SWIFT_VERSION=2.0 sh build.sh ios-static
+        REALM_SWIFT_VERSION=2.1 sh build.sh prelaunch-simulator
+        REALM_SWIFT_VERSION=2.1 sh build.sh test-ios-static
+        REALM_SWIFT_VERSION=2.1 sh build.sh ios-static
         move_to_clean_dir build/ios/Realm.framework xcode-7
 
         zip --symlinks -r build/ios/realm-framework-ios.zip xcode-6 xcode-7
@@ -738,8 +864,8 @@ case "$COMMAND" in
         move_to_clean_dir build/ios-dynamic/Realm.framework xcode-6
         rm -rf build
 
-        REALM_SWIFT_VERSION=2.0 sh build.sh prelaunch-simulator
-        REALM_SWIFT_VERSION=2.0 sh build.sh ios-dynamic
+        REALM_SWIFT_VERSION=2.1 sh build.sh prelaunch-simulator
+        REALM_SWIFT_VERSION=2.1 sh build.sh ios-dynamic
         move_to_clean_dir build/ios-dynamic/Realm.framework xcode-7
 
         zip --symlinks -r build/ios-dynamic/realm-dynamic-framework-ios.zip xcode-6 xcode-7
@@ -747,7 +873,7 @@ case "$COMMAND" in
 
     "package-osx")
         cd tightdb_objc
-        REALM_SWIFT_VERSION=2.0 sh build.sh test-osx
+        REALM_SWIFT_VERSION=2.1 sh build.sh test-osx
 
         cd build/DerivedData/Realm/Build/Products/Release
         zip --symlinks -r realm-framework-osx.zip Realm.framework
@@ -755,28 +881,28 @@ case "$COMMAND" in
 
     "package-ios-swift")
         cd tightdb_objc
-        for version in 1.2 2.0; do
+        for version in 1.2 2.1; do
             rm -rf build/ios-dynamic
             REALM_SWIFT_VERSION=$version sh build.sh prelaunch-simulator
             REALM_SWIFT_VERSION=$version sh build.sh ios-swift
         done
 
         cd build/ios
-        zip --symlinks -r realm-swift-framework-ios.zip swift-1.2 swift-2.0
+        zip --symlinks -r realm-swift-framework-ios.zip swift-1.2 swift-2.1
         ;;
 
     "package-osx-swift")
         cd tightdb_objc
         REALM_SWIFT_VERSION=1.2 sh build.sh osx-swift
-        REALM_SWIFT_VERSION=2.0 sh build.sh osx-swift
+        REALM_SWIFT_VERSION=2.1 sh build.sh osx-swift
 
         cd build/osx
-        zip --symlinks -r realm-swift-framework-osx.zip swift-1.2 swift-2.0
+        zip --symlinks -r realm-swift-framework-osx.zip swift-1.2 swift-2.1
         ;;
 
     "package-watchos")
         cd tightdb_objc
-        REALM_SWIFT_VERSION=2.0 sh build.sh watchos
+        REALM_SWIFT_VERSION=2.1 sh build.sh watchos
 
         cd build/watchos
         zip --symlinks -r realm-framework-watchos.zip Realm.framework
@@ -784,7 +910,7 @@ case "$COMMAND" in
 
     "package-watchos-swift")
         cd tightdb_objc
-        REALM_SWIFT_VERSION=2.0 sh build.sh watchos-swift
+        REALM_SWIFT_VERSION=2.1 sh build.sh watchos-swift
 
         cd build/watchos
         zip --symlinks -r realm-swift-framework-watchos.zip RealmSwift.framework Realm.framework
@@ -857,7 +983,7 @@ case "$COMMAND" in
             unzip ${WORKSPACE}/realm-examples.zip
             cd examples
             if [[ "${LANG}" == "objc" ]]; then
-                rm -rf ios/swift-1.2 ios/swift-2.0
+                rm -rf ios/swift-1.2 ios/swift-2.1
             else
                 rm -rf ios/objc ios/rubymotion osx
             fi
--- DTFLogger-Demo/DTFLogger-Demo/Pods/Realm/include/Realm/RLMRealm.h
+++ DTFLogger-Demo/DTFLogger-Demo/Pods/Realm/include/Realm/RLMRealm.h
@@ -47,10 +47,9 @@ RLM_ASSUME_NONNULL_BEGIN
  */
 
 @interface RLMRealm : NSObject
-/**---------------------------------------------------------------------------------------
- *  @name Creating & Initializing a Realm
- * ---------------------------------------------------------------------------------------
- */
+
+#pragma mark - Creating & Initializing a Realm
+
 /**
  Obtains an instance of the default Realm.
 
@@ -204,6 +203,11 @@ RLM_ASSUME_NONNULL_BEGIN
  */
 @property (nonatomic, readonly) RLMRealmConfiguration *configuration;
 
+/**
+ Indicates if this Realm contains any objects.
+ */
+@property (nonatomic, readonly) BOOL isEmpty;
+
 /**---------------------------------------------------------------------------------------
  *  @name Default Realm Path
  * ---------------------------------------------------------------------------------------
@@ -233,12 +237,11 @@ RLM_ASSUME_NONNULL_BEGIN
 
 #pragma mark - Notifications
 
+/// Block to run when the data in a Realm was modified.
 typedef void(^RLMNotificationBlock)(NSString *notification, RLMRealm *realm);
 
-/**---------------------------------------------------------------------------------------
- *  @name Receiving Notification when a Realm Changes
- * ---------------------------------------------------------------------------------------
- */
+
+#pragma mark - Receiving Notification when a Realm Changes
 
 /**
  Add a notification handler for changes in this RLMRealm.
@@ -272,10 +275,8 @@ typedef void(^RLMNotificationBlock)(NSString *notification, RLMRealm *realm);
 
 #pragma mark - Transactions
 
-/**---------------------------------------------------------------------------------------
- *  @name Writing to a Realm
- * ---------------------------------------------------------------------------------------
- */
+
+#pragma mark - Writing to a Realm
 
 /**
  Begins a write transaction in an `RLMRealm`.
@@ -305,7 +306,22 @@ typedef void(^RLMNotificationBlock)(NSString *notification, RLMRealm *realm);
 
  Calling this when not in a write transaction will throw an exception.
  */
-- (void)commitWriteTransaction;
+- (void)commitWriteTransaction RLM_SWIFT_UNAVAILABLE("");
+
+/**
+ Commits all writes operations in the current write transaction.
+
+ After this is called the `RLMRealm` reverts back to being read-only.
+
+ Calling this when not in a write transaction will throw an exception.
+
+ @param error If an error occurs, upon return contains an `NSError` object
+              that describes the problem. If you are not interested in
+              possible errors, pass in `NULL`.
+
+ @return Whether the transaction succeeded.
+ */
+- (BOOL)commitWriteTransaction:(NSError **)error;
 
 /**
  Revert all writes made in the current write transaction and end the transaction.
@@ -337,7 +353,19 @@ typedef void(^RLMNotificationBlock)(NSString *notification, RLMRealm *realm);
 /**
  Helper to perform a block within a transaction.
  */
-- (void)transactionWithBlock:(void(^)(void))block;
+- (void)transactionWithBlock:(void(^)(void))block RLM_SWIFT_UNAVAILABLE("");
+
+/**
+ Helper to perform a block within a transaction.
+
+ @param block The block to perform.
+ @param error If an error occurs, upon return contains an `NSError` object
+              that describes the problem. If you are not interested in
+              possible errors, pass in `NULL`.
+
+ @return Whether the transaction succeeded.
+ */
+- (BOOL)transactionWithBlock:(void(^)(void))block error:(NSError **)error;
 
 /**
  Update an `RLMRealm` and outstanding objects to point to the most recent data for this `RLMRealm`.
@@ -426,10 +454,9 @@ typedef void(^RLMNotificationBlock)(NSString *notification, RLMRealm *realm);
 
 #pragma mark - Accessing Objects
 
-/**---------------------------------------------------------------------------------------
- *  @name Adding and Removing Objects from a Realm
- * ---------------------------------------------------------------------------------------
- */
+
+#pragma mark - Adding and Removing Objects from a Realm
+
 /**
  Adds an object to be persisted it in this Realm.
 
@@ -625,40 +652,11 @@ typedef void (^RLMMigrationBlock)(RLMMigration *migration, uint64_t oldSchemaVer
  */
 + (NSError *)migrateRealmAtPath:(NSString *)realmPath encryptionKey:(NSData *)key DEPRECATED_MSG_ATTRIBUTE("Use +[RLMRealm migrateRealm:]");
 
-#pragma mark -
-
-//---------------------------------------------------------------------------------------
-//@name Named Object Storage and Retrieval
-//---------------------------------------------------------------------------------------
-//
-// Realm provides a top level key/value store for storing and accessing objects by NSString.
-// This system can be extended with the RLMKeyValueStore interface to create nested
-// namespaces as needed.
-
-// Retrieve a persisted object with an NSString.
-//
-// @usage RLMObject *object = RLMRealm.defaultRealm[@"name"];
-// @param key The NSString used to identify an object
-//
-// @return    RLMObject or nil if no object is stored for the given key.
-//
-//-(id)objectForKeyedSubscript:(id <NSCopying>)key;
-
-
-// Store an object with an NSString key.
-//
-// @usage RLMRealm.defaultRealm[@"name"] = object;
-// @param obj     The object to be stored.
-// @param key     The key that identifies the object to be used for future lookups.
-//
-//-(void)setObject:(RLMObject *)obj forKeyedSubscript:(id <NSCopying>)key;
-
-
 @end
 
-//
-// Notification token - holds onto the realm and the notification block
-//
+/**
+ Notification token - holds onto the realm and the notification block
+ */
 @interface RLMNotificationToken : NSObject
 @end
 
